cf8d287c68dfdbf325111e4a5348664222c797a9
thekirtv <149680777+thekirtv@users.noreply.github.com>
2024-05-24 14:16:24 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://gigan.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://gigan.nomoredomainswork.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
